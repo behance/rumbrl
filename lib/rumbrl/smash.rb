fa2ea2b77b29abf4a1643bd5892e33a3a7a05d9f
chr0n1x <heilong24@gmail.com>
2015-01-07 16:22:07 -0500

    Smash: face -> flatten

--- lib/rumbrl/smash.rb
+++ lib/rumbrl/smash.rb
@@ -1,13 +1,13 @@
 module Rumbrl
   # serializes & flattens hashes
   class Smash
-    def self.face(namespace: '', **target)
+    def self.flatten(namespace: '', **target)
       res = {}
       # flattens a hash
       target.each do |k, v|
         cur_nspace = build_namespace(namespace, k)
         if v.is_a? Hash
-          res.update face(**v, namespace: cur_nspace)
+          res.update flatten(**v, namespace: cur_nspace)
           next
         end
         res[cur_nspace.to_sym] = v
