447d965cffc54fe7183b9b9e31da8badd57a0517
chr0n1x <heilong24@gmail.com>
2015-01-07 15:05:23 -0500

    smash: hash serializer / flattener

---
+++ lib/rumbrl/smash.rb
@@ -0,0 +1,23 @@
+module Rumbrl
+  # serializes & flattens hashes
+  class Smash
+    def self.face(namespace: '', **target)
+      res = {}
+      # flattens a hash
+      target.each do |k,v|
+        cur_nspace = build_namespace(namespace, k)
+        if v.is_a? Hash
+          res.update face(**v, namespace: cur_nspace)
+          next
+        end
+        res[cur_nspace.to_sym] = v
+      end
+      res
+    end
+
+    def self.build_namespace(prev = '', cur = '')
+      prev = "#{prev}_" unless prev.empty?
+      "#{prev}#{cur.to_s}"
+    end
+  end
+end
