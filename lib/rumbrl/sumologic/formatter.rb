8aa8bde0f87895902ef331253a27cc16a9a4c8f4
chr0n1x <heilong24@gmail.com>
2015-07-08 11:50:52 -0400

    sumologic/formatter: slight refactor, also include timestamp

--- lib/rumbrl/sumologic/formatter.rb
+++ lib/rumbrl/sumologic/formatter.rb
@@ -6,13 +6,20 @@ module Rumbrl
     # Log4r formatter that turns hashable objects into KV log entries
     # with some decorations specifically for sumologic
     class Formatter < ::Rumbrl::KV::Formatter
-      def call(severity, _timestamp, prg, msg)
+      def call(severity, timestamp, prg, msg)
         return super if empty?(msg)
 
-        "APP_NAME='#{appname(prg)}' SEVERITY='#{severity}' #{format_msg(msg)}\n"
+        format_entry appname(prg), severity, timestamp, format_msg(msg)
       end
 
       private
+
+      def format_entry(app, sev, time, msg)
+        format(
+          "APP_NAME='%s' SEVERITY='%s' TIMESTAMP='%s' %s\n",
+          app, sev, time, msg
+        )
+      end
 
       def app_namespace
         ENV.fetch('LOG_APP_NAME', 'RubyApplication')
